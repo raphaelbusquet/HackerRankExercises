2596522cad1228ba2edafd853a4f4ee78600c384
Raphael Busquet <73319921+raphaelbusquet@users.noreply.github.com>
2024-05-05 22:34:09 +0100

    plusMinus problem

---
+++ plusMinus.js
@@ -0,0 +1,22 @@
+function plusMinus(arr){
+    let positive = 0;
+    let negative = 0;
+    let zero = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0){
+            positive += 1;
+        }
+        else if (arr[i] < 0){
+            negative += 1;
+        } else {
+            zero += 1;
+        }
+    }
+    
+    console.log((positive / arr.length).toFixed(6))
+    console.log((negative / arr.length).toFixed(6))
+    console.log((zero / arr.length).toFixed(6))
+}
+
+plusMinus([1, 1, 0, -1, -1])
+
