15f51f5bcb7401dc7bf74e80c9bf3a3b126ccd4b
Raphael Busquet <73319921+raphaelbusquet@users.noreply.github.com>
2024-05-13 12:01:25 +0100

    .

--- Min-MaxSum.js
+++ Min-MaxSum.js
@@ -7,19 +7,19 @@ function miniMaxSum(arr) {
     let smallest = arr[0];
     
     for (let i = 0; i < arr.length; i++){
-        if (arr[i] > largest){
-            largest = arr[i];
-        } else if (arr[i] < smallest){
-            smallest = arr[i];
-        }
+         if (arr[i] > largest){
+             largest = arr[i];
+         } else if (arr[i] < smallest){
+             smallest = arr[i];
+         }
     }
     
     for (let i = 0; i < arr.length; i++){
         sum += arr[i];
     }
     
-  minSum = sum - smallest
-  maxSum = sum - largest
-  
-  console.log(maxSum, minSum)
+    minSum = sum - largest;
+    maxSum = sum - smallest;
+    
+    console.log(minSum, maxSum)
 }
